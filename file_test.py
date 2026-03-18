40579dbfa291ce929b69cf514c9ee52ecb90d069
Nima Mrze <nimamrze@gmail.com>
2026-03-18 10:35:05 +0100

    fixed testfile

--- file_test.py
+++ file_test.py
@@ -8,10 +8,10 @@ def test_calc_substraction():
 	output = 2-4
 	assert output == -2
 
-def test_calc_multiply()
+def test_calc_multiply():
 	output = 2*4
 	assert output == 8
 
-def test_coucou()
+def test_coucou():
 	output = 'hello'
 	assert output == 'hello'
