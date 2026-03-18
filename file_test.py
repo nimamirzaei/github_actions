7c46107994cc5f3a8b3c340a4753a0be7bf95db5
Nima Mrze <nimamrze@gmail.com>
2026-03-18 10:50:07 +0100

    fixed typo

--- file_test.py
+++ file_test.py
@@ -3,7 +3,7 @@ def test_calc_addition():
     assert output == 6
 def test_calc_substraction():
     output = 2-4
-	assert output == -2
+    assert output == -2
 def test_calc_multiply():
     output = 2*4
     assert output == 8
